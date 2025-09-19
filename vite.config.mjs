5767af8a26f0a0872722dcf67ad5fa8785d8642b
Mohammed Zakir Nooraj <84661781+mdnooraj14@users.noreply.github.com>
2025-09-19 18:11:45 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-// vite.config.ts / vite.config.js
-// vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwind from '@tailwindcss/vite'   // <-- IMPORT THE PLUGIN
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwind()],          // <-- USE THE IMPORTED NAME
+  plugins: [react()],
 })
+vite.config.ts / vite.config.js
+vite.config.mjs
